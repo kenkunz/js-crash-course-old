d63e359088bbd7efcdc5573937199b4d9d0c0e67
Ken Kunz <ken.kunz@vibes.com>
2013-09-28 22:32:42 -0500

    execute code in inline-repl

---
+++ js/js-crash-course.js
@@ -0,0 +1,27 @@
+$(function() {
+
+  var _consoleLog = console.log;
+
+  $(".inline-repl").on("keypress", function(e) {
+    if (e.which === 18) {
+      var $codeEls = $(this).find("pre code");
+      var $code    = $codeEls.first();
+      var $console = $codeEls.last();
+
+      $code.blur();
+      var code = $code[0].innerText || $code[0].textContent;
+      $code.html( hljs.highlight('javascript', code).value );
+
+      console.log = function() {
+        var argStr = Array.prototype.join.call(arguments, " ");
+        $console.text($console.text() + argStr + "\n");
+        originalLog.apply(this, arguments);
+      }
+
+      $console.text("");
+      eval($code.text());
+      console.log = _consoleLog;
+    }
+  });
+
+});
